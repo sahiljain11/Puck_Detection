32114297547fddac109aaaaae03b9ca03581738d
sahiljain11 <sahilbot1@gmail.com>
2020-02-13 18:36:33 -0600

    Played around with a few filters in opencv

--- Air_Hockey/Blob_Detection.py
+++ Air_Hockey/Blob_Detection.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np
 
-#vidcap = cv2.VideoCapture('testing1.avi')
-vidcap = cv2.VideoCapture(0)
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+vidcap = cv2.VideoCapture('test_vid.MOV')
+#vidcap = cv2.VideoCapture(0)
+#fourcc = cv2.VideoWriter_fourcc(*'XVID')
+#out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
 
 count = 0
 
@@ -23,38 +23,57 @@ def draw (image):
     cv2.putText(image, 'Testing!', (0, 130), font, 0.7, (0, 255, 255), 1, cv2.LINE_AA)
 
 def image_operations (image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, mask = cv2.threshold(gray, 115, 255, cv2.THRESH_BINARY)
 
-    #how to explictly calling ROI (region of image)
-    image[0:120, 0:120] = [255, 255, 255]
+    #Not really too useful in this case but it's pretty good for clearing up an image
+    #guassian = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 115, 1)
+
+    #hue, saturation, and value
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower_blue = np.array([50, 0, 0])
+    upper_blue = np.array([120, 255, 255])
+
+    hsv_mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    res = cv2.bitwise_and(image, image, mask=hsv_mask)
+
+    #kernel = np.ones((15, 15), np.float32) / 255;
+    #smoothed = cv2.filter2D(res, -1, kernel)
+
+    #clearer = cv2.GaussianBlur(res, (15, 15), 0)
+    median = cv2.medianBlur(res, 15)
+
+    cv2.imshow("median", median)
+    #cv2.imshow("clearer", clearer)
+    #cv2.imshow("smoothed", smoothed)
+    return image
 
 while True:
 
     success, image = vidcap.read()
     count += 1
+    if success:
+        #out.write(image)       #For saving videos for data later
 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #out.write(image)       #For saving videos for data later
+        #draw(image)
+        #draw(gray)
 
-    #draw(image)
-    #draw(gray)
+        new_image = image_operations(image)
+        #cv2.imshow('image', image)
+        #cv2.imshow('new_image', new_image)
 
-    ret, mask = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+        #cv2.imwrite("frame%d.jpg" % count, image)
 
-    cv2.imshow('image', image)
-    cv2.imshow('gray', mask)
+        #im = cv2.imread("frame%d.jpg" % count, cv2.IMREAD_GRAYSCALE)
 
-    #cv2.imwrite("frame%d.jpg" % count, image)
+        #detector = cv2.SimpleBlobDetector()
+        #o = cv2.ORB_create()
+        #keypoints = o.detect(im, None)
 
-    #im = cv2.imread("frame%d.jpg" % count, cv2.IMREAD_GRAYSCALE)
+        ##Detect orangeish colors
+        #im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (250, 190, 88))
 
-    #detector = cv2.SimpleBlobDetector()
-    #o = cv2.ORB_create()
-    #keypoints = o.detect(im, None)
-
-    ##Detect orangeish colors
-    #im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (250, 190, 88))
-
-    #cv2.imshow("Keypoints", im_with_keypoints)
+        #cv2.imshow("Keypoints", im_with_keypoints)
 
     #if (count % 10 == 0):
     #    print('Finished with #%d' % count)
@@ -63,5 +82,8 @@ while True:
         break
 
 vidcap.release()
-out.release()
+#out.release()
 cv2.destroyAllWindows()
+
+# how to explictly calling ROI (region of image)
+# image[0:120, 0:120] = [255, 255, 255]

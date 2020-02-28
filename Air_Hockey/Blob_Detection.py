1bdc8164f3c1c1370f93d54bd51e9ea9909f3f35
sahiljain11 <sahilbot1@gmail.com>
2020-02-28 15:07:16 -0600

    Finished main functionality. adding convinence with argparse

--- Air_Hockey/Blob_Detection.py
+++ Air_Hockey/Blob_Detection.py
@@ -1,102 +1,121 @@
 import cv2
 import numpy as np
+import time
+import imutils
+import argparse
 
-vidcap = cv2.VideoCapture('test_vid.MOV')
-#vidcap = cv2.VideoCapture(0)
-#fourcc = cv2.VideoWriter_fourcc(*'XVID')
-#out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+ap = argparse.ArgumentParser()
+ap.add_argument("-s", "--stream", type=int, default=0, help="stream video from videostream. denote capture #")
+ap.add_argument("-w", "--write", type=str, default="output.avi", help="write video stream to file")
+ap.add_argument("-r", "--read", type=str, default="", help="read from existing video file")
 
+args = vars(ap.parse_args())
+
+if (args.get('read') != ""):
+    vidcap = cv2.VideoCapture(args.get('read'))
+else:
+    vidcap = cv2.VideoCapture(args.get("stream"))
+
+if (args.get('write') != ""):
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+
+time.sleep(2.0)     # Helps in loading up video processing
 count = 0
+blob_params = cv2.SimpleBlobDetector_Params()
 
-def draw (image):
-    #image, (x1, y1) (x2, y2), (b, g, r), width
-    cv2.line(image, (0, 0), (150, 150), (255, 0, 0) , 15)
+# Filter by Color
+blob_params.filterByColor = True
+blob_params.filterByArea = False
+blob_params.filterByConvexity = False
+blob_params.filterByInertia = False
+blob_params.filterByCircularity = False
 
-    #image, (top left x1, y1), (bottom right x2, y2), (b, g, r), width
-    #-1 denotes a filled in object
-    cv2.rectangle(image, (15, 25), (200, 150), (0, 0, 255), 5)
+detector = cv2.SimpleBlobDetector_create(blob_params)
+lower_blue = np.array([100, 50, 50])
+upper_blue = np.array([120, 255, 255])
 
-    #image, (center coords x, y), radius, (b, g, r), width
-    cv2.circle(image, (100, 63), 55, (0, 255, 0), -1)
 
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(image, 'Testing!', (0, 130), font, 0.7, (0, 255, 255), 1, cv2.LINE_AA)
-
-def image_operations (image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    ret, mask = cv2.threshold(gray, 115, 255, cv2.THRESH_BINARY)
-
-    #Not really too useful in this case but it's pretty good for clearing up an image
-    #guassian = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 115, 1)
-
-    #hue, saturation, and value
+def image_operations(image):
+    # hue, saturation, and value
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    lower_blue = np.array([50, 0, 0])
-    upper_blue = np.array([120, 255, 255])
 
     hsv_mask = cv2.inRange(hsv, lower_blue, upper_blue)
     res = cv2.bitwise_and(image, image, mask=hsv_mask)
 
-    #different filters to produce a better image
-    #kernel = np.ones((15, 15), np.float32) / 255;
-    #smoothed = cv2.filter2D(res, -1, kernel)
-    #clearer = cv2.GaussianBlur(res, (15, 15), 0)
-    median = cv2.medianBlur(res, 15)       #best one
+    median = cv2.medianBlur(res, 15)
 
-    kernel = np.ones((5, 5), np.uint8)
-    #erosion = cv2.erode(mask, kernel, iterations=1)
-    #dilation = cv2.dilate(mask, kernel, iterations=1)
+    kernel = np.ones((15, 15), np.float32) / 255;
+    smoothed = cv2.filter2D(median, -1, kernel)
 
-    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    #closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    # cv2.imshow("median", median)
+    # cv2.imshow("smoothed", smoothed)
 
-    #cv2.imshow("opening", opening)
-    #cv2.imshow("median", median)
+    return smoothed
 
-    #edges and gradients
-    #laplacian = cv2.Laplacian(image, cv2.CV_64F)
-    #sobelx = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=5)
-    #sobely = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5)
 
-    #edges = cv2.Canny(image, 200, 200)      #Edge detection
-    return image
+def different_operations(image):
+    blur = cv2.GaussianBlur(image, (11, 11), 0)
+    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)  # actually gets the puck
+    mask = cv2.erode(mask, None, iterations=2)  # removes any excess blobs that may be detected
+    mask = cv2.dilate(mask, None, iterations=2)  # also removes any excess blobs that may be detected
+
+    cv2.imshow("mask", mask)
+
+    return mask
+
+
+def detection(original, image):
+    inverse_im = cv2.bitwise_not(image)
+
+    keypoints = detector.detect(inverse_im)
+
+    print("-----------------------------------")
+    print("num of blobs: ", len(keypoints))
+    for point in keypoints:
+        print(point.pt[0], " ", point.pt[1])
+    print("-----------------------------------")
+
+    im_with_keypoints = cv2.drawKeypoints(original, keypoints, np.array([]),
+                                          (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    return im_with_keypoints
+
 
 while True:
 
+    file = open("debug.txt", "+w")
     success, image = vidcap.read()
     count += 1
     if success:
-        #out.write(image)       #For saving videos for data later
+        # out.write(image)       #For saving videos for data later
 
-        #draw(image)
-        #draw(gray)
+        image = imutils.resize(image, width=600)  # increases FPS
+        # new_image = image_operations(image)
+        new_image = different_operations(image)
 
-        new_image = image_operations(image)
-        #cv2.imshow('image', image)
-        #cv2.imshow('new_image', new_image)
+        blobs = detection(image, new_image)
+        cv2.imshow("blobs", blobs)
 
-        #cv2.imwrite("frame%d.jpg" % count, image)
+        # file.write("--------------------------------\n")
+        # file.write(str(new_image) + "\n")
+        # file.write("--------------------------------\n")
 
-        #im = cv2.imread("frame%d.jpg" % count, cv2.IMREAD_GRAYSCALE)
+        # cv2.imshow('image', image)
+        # cv2.imshow('new_image', new_image)
 
-        #detector = cv2.SimpleBlobDetector()
-        #o = cv2.ORB_create()
-        #keypoints = o.detect(im, None)
+        # cv2.imwrite("frame%d.jpg" % count, image)
 
-        ##Detect orangeish colors
-        #im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (250, 190, 88))
+        # im = cv2.imread("frame%d.jpg" % count, cv2.IMREAD_GRAYSCALE)
 
-        #cv2.imshow("Keypoints", im_with_keypoints)
-
-    #if (count % 10 == 0):
+    # if (count % 10 == 0):
     #    print('Finished with #%d' % count)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        file.close()
         break
 
 vidcap.release()
-#out.release()
+# out.release()
 cv2.destroyAllWindows()
-
-# how to explictly calling ROI (region of image)
-# image[0:120, 0:120] = [255, 255, 255]
